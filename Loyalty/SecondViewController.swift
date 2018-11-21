55a0704c4baf6ab526cfaa483fc06aa3b1d19ed9
AlainKRSS <cvt.alain@gmail.com>
2018-11-21 14:18:29 +0100

    feat(): set initial tabbed app

---
+++ Loyalty/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Loyalty
+//
+//  Created by Alain on 21/11/2018.
+//  Copyright © 2018 iOSHetic. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
