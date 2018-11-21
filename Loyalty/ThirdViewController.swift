f03264f015d1dcdefa650ff0a486ff18d23f7352
AlainKRSS <cvt.alain@gmail.com>
2018-11-21 14:35:01 +0100

    feat(views): create all main views

---
+++ Loyalty/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  Loyalty
+//
+//  Created by Alain on 21/11/2018.
+//  Copyright © 2018 iOSHetic. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
