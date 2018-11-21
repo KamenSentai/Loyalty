94603d8952da60119bc10322366ab0d1610ddd8b
AlainKRSS <cvt.alain@gmail.com>
2018-11-21 20:01:17 +0100

    feat(navigation): build navigation

--- Loyalty/HomeViewController.swift
+++ Loyalty/HomeViewController.swift
@@ -15,6 +15,10 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addCategoryAction(_ sender: Any) {
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle : nil)
+        let addCategoryViewController = storyboard.instantiateViewController(withIdentifier: "AddCategoryViewController") as! AddCategoryViewController
+        navigationController?.pushViewController(addCategoryViewController, animated: true)
+    }
 }
 
