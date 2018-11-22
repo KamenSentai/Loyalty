4cb4e6fdfa1190c72ee506345a252fe31d6b99fd
AlainKRSS <cvt.alain@gmail.com>
2018-11-22 12:57:07 +0100

    feat(icon): add missing icons

---
+++ Loyalty/AddCardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddCategoryViewController.swift
+//  Loyalty
+//
+//  Created by Alain on 21/11/2018.
+//  Copyright © 2018 iOSHetic. All rights reserved.
+//
+
+import UIKit
+
+class AddCategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.topItem?.title = "Catégories"
+    }
+
+}
